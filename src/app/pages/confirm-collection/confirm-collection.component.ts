8694d436ec655c33b81b8df423d2f00b0429fe5b
Vinicius Reis <vinireis010305@gmail.com>
2023-11-22 22:33:40 -0300

    Update delivery address in confirm-collection component

--- src/app/pages/confirm-collection/confirm-collection.component.ts
+++ src/app/pages/confirm-collection/confirm-collection.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-confirm-collection',
   templateUrl: './confirm-collection.component.html',
-  styleUrls: ['./confirm-collection.component.css']
+  styleUrls: ['./confirm-collection.component.css'],
 })
 export class ConfirmCollectionComponent {
+  address = 'Rua do Ouro';
+  number = '123';
+  complement = 'Apto 101';
+  neighborhood = 'Serra';
+  city = 'Belo Horizonte';
+  state = 'MG';
 
+  ngOnInit(): void {
+    const scheduleData = sessionStorage.getItem('scheduleForm');
+    if (scheduleData) {
+      const parsedScheduleData = JSON.parse(scheduleData);
+      this.address = parsedScheduleData.address;
+      this.number = parsedScheduleData.number;
+      this.complement = parsedScheduleData.complement;
+      this.neighborhood = parsedScheduleData.neighborhood;
+      this.city = parsedScheduleData.city;
+      this.state = parsedScheduleData.state;
+      console.log(parsedScheduleData.complement)
+    }
+  }
 }
